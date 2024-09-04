6fdb047976900748285feaccf1a2f4a10aad3ea6
Santiago <san.agarie@gmail.com>
2024-09-03 21:46:38 -0300

    Primera implementacion de DTO(a clase turno)

--- src/main/java/com/dh/clinica/controller/TurnoController.java
+++ src/main/java/com/dh/clinica/controller/TurnoController.java
@@ -1,5 +1,8 @@
 package com.dh.clinica.controller;
 
+import com.dh.clinica.dto.request.TurnoModificarDto;
+import com.dh.clinica.dto.request.TurnoRequestDto;
+import com.dh.clinica.dto.response.TurnoResponseDto;
 import com.dh.clinica.entity.Turno;
 import com.dh.clinica.service.impl.TurnoService;
 import org.springframework.http.HttpStatus;
@@ -19,8 +22,8 @@ public class TurnoController {
     }
 
     @PostMapping("/guardar")
-    public ResponseEntity<?> guardarTurno(@RequestBody Turno turno){
-        Turno turnoAGuardar = turnoService.guardarTurno(turno);
+    public ResponseEntity<?> guardarTurno(@RequestBody TurnoRequestDto turnoRequestDto){
+        TurnoResponseDto turnoAGuardar = turnoService.guardarTurno(turnoRequestDto);
         if (turnoAGuardar != null){
             return ResponseEntity.ok(turnoAGuardar);
         } else {
@@ -30,7 +33,7 @@ public class TurnoController {
 
     @GetMapping("/buscar/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Integer id){
-        Optional<Turno> turno = turnoService.buscarPorId(id);
+        Optional<TurnoResponseDto> turno = turnoService.buscarPorId(id);
         if(turno.isPresent()){
             return ResponseEntity.ok(turno.get());
         }else {
@@ -39,24 +42,19 @@ public class TurnoController {
     }
 
     @GetMapping("/buscartodos")
-    public ResponseEntity<List<Turno>> buscartodos(){
+    public ResponseEntity<List<TurnoResponseDto>> buscartodos(){
         return ResponseEntity.ok(turnoService.buscarTodos());
     }
 
     @PutMapping("/modificar")
-    public ResponseEntity<?> modificarTurno(@RequestBody Turno turno){
-        Optional<Turno> turnoEncontrado = turnoService.buscarPorId(turno.getId());
-        if(turnoEncontrado.isPresent()){
-            turnoService.modificarTurno(turno);
-            return ResponseEntity.ok("El turno fue modificado");
-        }else{
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<?> modificarTurno(@RequestBody TurnoModificarDto turnoModificarDto){
+       turnoService.modificarTurno(turnoModificarDto);
+       return ResponseEntity.ok("{\"mensaje\": \"El paciente fue modificado\"}");
     }
 
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<?> eliminarTurno(@PathVariable Integer id){
-        Optional<Turno> turno = turnoService.buscarPorId(id);
+        Optional<TurnoResponseDto> turno = turnoService.buscarPorId(id);
         if(turno.isPresent()){
             turnoService.eliminarTurno(id);
             return ResponseEntity.ok("El paciente fue eliminado");
--- src/main/java/com/dh/clinica/service/impl/TurnoService.java
+++ src/main/java/com/dh/clinica/service/impl/TurnoService.java
@@ -1,5 +1,10 @@
 package com.dh.clinica.service.impl;
 
+import com.dh.clinica.dto.request.TurnoModificarDto;
+import com.dh.clinica.dto.request.TurnoRequestDto;
+import com.dh.clinica.dto.response.OdontologoResponseDto;
+import com.dh.clinica.dto.response.PacienteResponseDto;
+import com.dh.clinica.dto.response.TurnoResponseDto;
 import com.dh.clinica.entity.Odontologo;
 import com.dh.clinica.entity.Paciente;
 import com.dh.clinica.entity.Turno;
@@ -7,6 +12,8 @@ import com.dh.clinica.repository.ITurnoRepository;
 import com.dh.clinica.service.ITurnoService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,35 +30,58 @@ public class TurnoService implements ITurnoService {
     }
 
     @Override
-    public Turno guardarTurno(Turno turno) {
-        Optional<Paciente> paciente = pacienteService.buscarPorId(turno.getPaciente().getId());
-        Optional<Odontologo> odontologo = odontologoService.buscarPorId(turno.getOdontologo().getId());
-        Turno turnoARetornar = null;
+    public TurnoResponseDto guardarTurno(TurnoRequestDto turnoRequestDto) {
+        Optional<Paciente> paciente = pacienteService.buscarPorId(turnoRequestDto.getPaciente_id());
+        Optional<Odontologo> odontologo = odontologoService.buscarPorId(turnoRequestDto.getOdontologo_id());
+        Turno turno = new Turno();
+        Turno turnoDesdeDB = null;
+        TurnoResponseDto turnoARetornar = null;
         if(paciente.isPresent() && odontologo.isPresent()){
+            // mapear el turnoRequestDto aa turno
             turno.setPaciente(paciente.get());
             turno.setOdontologo(odontologo.get());
-            turnoARetornar = turnoRepository.save(turno);
+            turno.setFecha(LocalDate.parse(turnoRequestDto.getFecha()));
+            // persistimos el turno
+            turnoDesdeDB = turnoRepository.save(turno);
+
+            // mapear el turnoDesdeDB a turnoResponseDto
+
+            turnoARetornar = convertirTurnoAResponse(turnoDesdeDB);
+
         }
         return turnoARetornar;
     }
 
     @Override
-    public Optional<Turno> buscarPorId(Integer id) {
-        return turnoRepository.findById(id);
+    public Optional<TurnoResponseDto> buscarPorId(Integer id) {
+        Optional<Turno> turnoDesdeLaDB = turnoRepository.findById(id);
+        TurnoResponseDto turnoResponseDto = null;
+        if(turnoDesdeLaDB.isPresent()){
+            turnoResponseDto = convertirTurnoAResponse(turnoDesdeLaDB.get());
+        }
+        return Optional.ofNullable(turnoResponseDto);
     }
 
     @Override
-    public List<Turno> buscarTodos() {
-        return turnoRepository.findAll();
+    public List<TurnoResponseDto> buscarTodos() {
+        List<Turno> turnos = turnoRepository.findAll();
+        List<TurnoResponseDto> turnoRespuesta = new ArrayList<>();
+        for (Turno t: turnos){
+            TurnoResponseDto turnoAuxiliar = convertirTurnoAResponse(t);
+            turnoRespuesta.add(turnoAuxiliar);
+        }
+        return turnoRespuesta;
     }
 
     @Override
-    public void modificarTurno(Turno turno) {
-        Optional<Paciente> paciente = pacienteService.buscarPorId(turno.getPaciente().getId());
-        Optional<Odontologo> odontologo = odontologoService.buscarPorId(turno.getOdontologo().getId());
+    public void modificarTurno(TurnoModificarDto turnoModificarDto) {
+        Optional<Paciente> paciente = pacienteService.buscarPorId(turnoModificarDto.getPaciente_id());
+        Optional<Odontologo> odontologo = odontologoService.buscarPorId(turnoModificarDto.getOdontologo_id());
+        Turno turno = null;
         if(paciente.isPresent() && odontologo.isPresent()){
-            turno.setPaciente(paciente.get());
-            turno.setOdontologo(odontologo.get());
+            turno = new Turno(turnoModificarDto.getId(), paciente.get(), odontologo.get(),
+                    LocalDate.parse(turnoModificarDto.getFecha()));
+            // voy a persistir el turno
             turnoRepository.save(turno);
         }
     }
@@ -59,5 +89,24 @@ public class TurnoService implements ITurnoService {
     @Override
     public void eliminarTurno(Integer id) {
         turnoRepository.deleteById(id);
+    }
+
+    private TurnoResponseDto convertirTurnoAResponse(Turno turnoDesdeDB){
+        OdontologoResponseDto odontologoResponseDto = new OdontologoResponseDto(
+                turnoDesdeDB.getOdontologo().getId(), turnoDesdeDB.getOdontologo().getNroMatricula(),
+                turnoDesdeDB.getOdontologo().getNombre(), turnoDesdeDB.getOdontologo().getApellido()
+        );
+
+        PacienteResponseDto pacienteResponseDto = new PacienteResponseDto(
+                turnoDesdeDB.getPaciente().getId(), turnoDesdeDB.getPaciente().getNombre(),
+                turnoDesdeDB.getPaciente().getApellido(), turnoDesdeDB.getPaciente().getDni()
+        );
+
+        TurnoResponseDto turnoARetornar = new TurnoResponseDto(
+                turnoDesdeDB.getId(), odontologoResponseDto, pacienteResponseDto,
+                turnoDesdeDB.getFecha().toString()
+        );
+
+        return turnoARetornar;
     }
 }
